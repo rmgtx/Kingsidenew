e37a3bb46277971d623673e4f5fca3c5c8440f14
rmg_tx <agateyte@gmail.com>
2026-01-26 17:13:08 -0600

    social fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Kingsidenew/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
